3cbb9ba58541e5a9eba5b6db84bc0459dcc2edc6
Jason Crease <jason.crease@red-gate.com>
2014-02-25 13:27:21 +0000

    made it find corners well

--- FoosVision/Engine/TableFinder.cs
+++ FoosVision/Engine/TableFinder.cs
@@ -43,51 +43,80 @@ namespace Engine
 
         public System.Drawing.PointF[] GetTableCorners(Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> TableImage)
         {
-            DownImage = TableImage.Convert<Bgr, Byte>().PyrDown().PyrDown();
-            GrayImage = TableImage.Convert<Gray, Byte>().PyrDown().PyrDown();
+            int scale = 2;
+            int offset = 4;
+            DownImage = TableImage.Convert<Bgr, Byte>().PyrDown();
+            //GrayImage = TableImage.Convert<Gray, Byte>().PyrDown().PyrDown();
 
             //ThresholdImage = ImageProcess.ThresholdHsv(DownImage, 0, 23, 40, 143, 118, 256);    // For wood
             ThresholdImage = ImageProcess.ThresholdHsv(DownImage, 22, 89, 33, 135, 42, 201);  // For green
-            ThresholdImage = ThresholdImage.Erode(8);
+            ThresholdImage = ThresholdImage.Erode(1);
+            ThresholdImage = ThresholdImage.Dilate(1);
 
-            //Image<Gray, Byte> FilteredImage = GrayImage.Copy(ThresholdImage);
+            PointF topLeft = PointF.Empty;
+            PointF topRight = PointF.Empty;
+            PointF bottomLeft = PointF.Empty;
+            PointF bottomRight = PointF.Empty;
 
-            // Do canny filter
-            CannyImage = ThresholdImage.Canny(100.0, 60.0);
+            int widthOver2 = ThresholdImage.Width / 2;
+            int heightOver2 = ThresholdImage.Height / 2;
 
-            List<PointF> points = new List<PointF>();
+            for (int startY = 0; startY < ThresholdImage.Height; startY++)
+                for (int x = 0; x < startY; x++)
+                {
+                    int y = startY - x;
+                    if (ThresholdImage.Data[y, x, 0] > 0)
+                    {
+                        topLeft = new PointF((x + offset) * scale, (y - offset) * scale);
+                        goto next1;
+                    }
+                }
+            next1:
 
-            for (int x = 0; x < CannyImage.Width; x++)
-                for (int y = 0; y < CannyImage.Height; y++)
-                    if (CannyImage.Data[y, x, 0] > 100) points.Add(new PointF(x, y));
+            for (int startY = 0; startY < ThresholdImage.Height; startY++)
+                for (int x = 0; x < startY; x++)
+                {
+                    int y = startY - x;
+                    int x2 = ThresholdImage.Width - x - 1;
+                    if (ThresholdImage.Data[y, x2, 0] > 0)
+                    {
+                        topRight = new PointF((x2 - offset) * scale, (y - offset) * scale);
+                        goto next2;
+                    }
+                }
+            next2:
 
-            var box = PointCollection.MinAreaRect(points.ToArray());
+            for (int startY = 0; startY < ThresholdImage.Height; startY++)
+                for (int x = 0; x < startY; x++)
+                {
+                    int y = ThresholdImage.Height - (startY - x) - 1;
+                    if (ThresholdImage.Data[y, x, 0] > 0)
+                    {
+                        bottomLeft = new PointF((x + offset) * scale, (y + offset) * scale);
+                        goto next3;
+                    }
+                }
+            next3:
 
-            box.center.X *= 4;
-            box.center.Y *= 4;
-            box.size.Height *= 4;
-            box.size.Width *= 4;
+            for (int startY = 0; startY < ThresholdImage.Height; startY++)
+                for (int x = 0; x < startY; x++)
+                {
+                    int y = ThresholdImage.Height - (startY - x) - 1;
+                    int x2 = ThresholdImage.Width - x - 1;
+                    if (ThresholdImage.Data[y, x2, 0] > 0)
+                    {
+                        bottomRight = new PointF((x2 - offset) * scale, (y + offset) * scale);
+                        goto next4;
+                    }
+                }
+            next4:
 
-            LinesImage = TableImage.Copy();
-            LinesImage.Draw(box, new Bgr(Color.GreenYellow), 10);
+            ThresholdImage.Draw(new CircleF(topLeft, 10), new Gray(190), 4);
+            ThresholdImage.Draw(new CircleF(topRight, 11), new Gray(190), 6);
+            ThresholdImage.Draw(new CircleF(bottomLeft, 12), new Gray(190), 8);
+            ThresholdImage.Draw(new CircleF(bottomRight, 13), new Gray(190), 10);
 
-            return null;
-
-            //// Do Edge finder
-            Lines = CannyImage.HoughLinesBinary(
-                1, //Distance resolution in pixel-related units
-               Math.PI / 360, //Angle resolution measured in radians.
-                50, //threshold
-                30, //min Line width
-                40 //gap between lines
-                )[0]; //Get the lines from the first channel
-
-
-            //LinesImage = FilteredImage.Convert<Bgr, byte>();
-            foreach (LineSegment2D line in Lines)
-                LinesImage.Draw(line, new Bgr(System.Drawing.Color.Red), 2);
-
-            return null;
+            return new PointF[] { topLeft, topRight, bottomLeft, bottomRight };
         }
     }
 }
