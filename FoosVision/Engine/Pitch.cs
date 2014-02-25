de8d89e36ad729f3da51b662702e10f27fe69169
Jason Crease <jasoncrease@gmail.com>
2014-02-25 22:05:31 +0000

    find homography

--- FoosVision/Engine/Pitch.cs
+++ FoosVision/Engine/Pitch.cs
@@ -51,35 +51,44 @@ namespace Engine
         private PointF m_OldBLPoint = PointF.Empty;
         private PointF m_OldBRPoint = PointF.Empty;
 
+        public HomographyMatrix WarpMatrix { get { return m_WarpMat; } }
+        private HomographyMatrix m_WarpMat;
+
         public void Update(Image<Bgr, byte> nowImage)
         {
             FindPitchCorners(nowImage);
 
-            if (m_OldTLPoint == Point.Empty)
-            {
-                m_OldBLPoint = BottomLeft;
-                m_OldBRPoint = BottomRight;
-                m_OldTLPoint = TopLeft;
-                m_OldTRPoint = TopRight;
-            }
-            else
-            {
-                const float rate = 0.2f;
-                BottomLeft.X = (BottomLeft.X * rate) + (m_OldBLPoint.X * (1 - rate));
-                BottomLeft.Y = (BottomLeft.Y * rate) + (m_OldBLPoint.Y * (1 - rate));
-                BottomRight.X = (BottomRight.X * rate) + (m_OldBRPoint.X * (1 - rate));
-                BottomRight.Y = (BottomRight.Y * rate) + (m_OldBRPoint.Y * (1 - rate));
-                TopLeft.X = (TopLeft.X * rate) + (m_OldTLPoint.X * (1 - rate));
-                TopLeft.Y = (TopLeft.Y * rate) + (m_OldTLPoint.Y * (1 - rate));
-                TopRight.X = (TopRight.X * rate) + (m_OldTRPoint.X * (1 - rate));
-                TopRight.Y = (TopRight.Y * rate) + (m_OldTRPoint.Y * (1 - rate));
+            //if (m_OldTLPoint == Point.Empty)    
+            //{
+            //    m_OldBLPoint = BottomLeft;
+            //    m_OldBRPoint = BottomRight;
+            //    m_OldTLPoint = TopLeft;
+            //    m_OldTRPoint = TopRight;
+            //}
+            //else
+            //{
+            //    const float rate = 0.2f;
+            //    BottomLeft.X = (BottomLeft.X * rate) + (m_OldBLPoint.X * (1 - rate));
+            //    BottomLeft.Y = (BottomLeft.Y * rate) + (m_OldBLPoint.Y * (1 - rate));
+            //    BottomRight.X = (BottomRight.X * rate) + (m_OldBRPoint.X * (1 - rate));
+            //    BottomRight.Y = (BottomRight.Y * rate) + (m_OldBRPoint.Y * (1 - rate));
+            //    TopLeft.X = (TopLeft.X * rate) + (m_OldTLPoint.X * (1 - rate));
+            //    TopLeft.Y = (TopLeft.Y * rate) + (m_OldTLPoint.Y * (1 - rate));
+            //    TopRight.X = (TopRight.X * rate) + (m_OldTRPoint.X * (1 - rate));
+            //    TopRight.Y = (TopRight.Y * rate) + (m_OldTRPoint.Y * (1 - rate));
 
-                m_OldBLPoint = BottomLeft;
-                m_OldBRPoint = BottomRight;
-                m_OldTLPoint = TopLeft;
-                m_OldTRPoint = TopRight;
-            }
+            //    m_OldBLPoint = BottomLeft;
+            //    m_OldBRPoint = BottomRight;
+            //    m_OldTLPoint = TopLeft;
+            //    m_OldTRPoint = TopRight;
+            //}
+
+            PointF[] sourcePoints = { BottomLeft, BottomRight, TopLeft, TopRight };
+            PointF[] destPoints = { new PointF(300, 700), new PointF(900, 700), new PointF(300, 100), new PointF(900, 100) };
+
+            m_WarpMat = CameraCalibration.GetPerspectiveTransform(sourcePoints, destPoints);
         }
+
 
         private void FindPitchCorners(Image<Bgr, byte> nowImage)
         {
