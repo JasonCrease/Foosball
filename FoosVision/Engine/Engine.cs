0b45c7565e98e1bbc5f96315117552dab1041e70
Jason Crease <jason.crease@red-gate.com>
2014-02-26 18:58:34 +0000

    nicer overlays

--- FoosVision/Engine/Engine.cs
+++ FoosVision/Engine/Engine.cs
@@ -33,24 +33,24 @@ namespace Engine
             // Updating 
 
             m_Ball.Update(tableImage);
-            if (m_FrameNumber % 10 == 0)
+            if (m_FrameNumber % 5 == 0)
                 m_Pitch.Update(tableImage);
             m_Ball.SetBallRealPosition(m_Pitch.WarpMatrix);
             Stats.AddBall(m_Ball);
 
             // Drawing 
 
-            if (m_Ball.Pos.X > 0 && m_Ball.Pos.Y > 0 && m_Ball.Pos.X < 1280 && m_Ball.Pos.Y < 720)
-                DebugImage.Draw(new CircleF(m_Ball.Pos, 10), new Bgr(70, 115, 255), 2);
+            if (m_Ball.Pos.X > 0 && m_Ball.Pos.Y > 0 && m_Ball.Pos.X < tableImage.Width && m_Ball.Pos.Y < tableImage.Height)
+                DebugImage.Draw(new CircleF(m_Ball.Pos, 12), new Bgr(Color.MediumBlue), 5);
 
             DebugImage.Draw(new CircleF(m_Pitch.TopLeft, 12), new Bgr(0, 0, 200), 4);
             DebugImage.Draw(new CircleF(m_Pitch.TopRight, 12), new Bgr(0, 100, 200), 4);
             DebugImage.Draw(new CircleF(m_Pitch.BottomLeft, 12), new Bgr(100, 0, 200), 4);
             DebugImage.Draw(new CircleF(m_Pitch.BottomRight, 12), new Bgr(0, 200, 200), 4);
 
-            //DebugImage = DebugImage.WarpPerspective(m_Pitch.WarpMatrix, 1280, 720,
+            //DebugImage = DebugImage.WarpPerspective(m_Pitch.WarpMatrix, 1205, 800,
             //    Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC,
-            //    Emgu.CV.CvEnum.WARP.CV_WARP_FILL_OUTLIERS, new Bgr(Color.Aqua));
+            //    Emgu.CV.CvEnum.WARP.CV_WARP_FILL_OUTLIERS, new Bgr(Color.Wheat));
 
             m_FrameNumber++;
         }
