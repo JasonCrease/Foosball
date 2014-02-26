1eec53205516545ef61d1207c42e2f6a24410e25
Jason Crease <jason.crease@red-gate.com>
2014-02-26 11:38:45 +0000

    fixed COM. variable renames, removed dud line, erode 3 times

--- FoosVision/Engine/ImageProcess.cs
+++ FoosVision/Engine/ImageProcess.cs
@@ -12,26 +12,24 @@ namespace Engine
     {
         public static Image<Gray, byte> ThresholdHsv(Image<Bgr, Byte> image, int hmin, int hmax, int smin, int smax, int vmin, int vmax)
         {
-            //TableImage = new Image<Bgr, byte>(TableImagePath).Resize(1280, 720, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC, false);
-
             Image<Hsv, Byte> hsvImg = image.Convert<Hsv, Byte>();
-            Image<Gray, Byte>[] Channels = hsvImg.Split();
-            Channels[0] = Channels[0].InRange(new Gray(hmin), new Gray(hmax));
-            Channels[1] = Channels[1].InRange(new Gray(smin), new Gray(smax));
-            Channels[2] = Channels[2].InRange(new Gray(vmin), new Gray(vmax));
+            Image<Gray, Byte>[] channels = hsvImg.Split();
+            channels[0] = channels[0].InRange(new Gray(hmin), new Gray(hmax));
+            channels[1] = channels[1].InRange(new Gray(smin), new Gray(smax));
+            channels[2] = channels[2].InRange(new Gray(vmin), new Gray(vmax));
 
-            return Channels[0].And(Channels[1]).And(Channels[2]);
+            return channels[0].And(channels[1]).And(channels[2]);
         }
 
         public static PointF GetCentreOfMass(Image<Gray, byte> image)
         {
-            image = image.Erode(2);
+            image = image.Erode(3);
 
             long imageWidth = image.Width;
             long imageHeight = image.Height;
             long totalMassX = 0;
             long totalMassY = 0;
-            long totalMass = 1;
+            long totalMass = 0;
 
             for (int x = 0; x < imageWidth; x += 1)
                 for (int y = 0; y < imageHeight; y += 1)
@@ -44,13 +42,13 @@ namespace Engine
                     }
                 }
 
+            if (totalMass < 3)          // heuristically, this means too few pixels to come to a good conclusion
+                return PointF.Empty;
+
             float comX = totalMassX / totalMass;
             float comY = totalMassY / totalMass;
 
-            if (totalMass < 3)      // heuristically, this means too few pixels to come to a good conclusion
-                return new PointF(0, 0);
-            else
-                return new PointF(comX, comY);
+            return new PointF(comX, comY);
         }
     }
 }
